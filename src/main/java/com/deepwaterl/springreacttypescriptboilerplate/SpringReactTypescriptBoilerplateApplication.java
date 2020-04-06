5b8ea8244a71aa52bb40c79c6dc82ccccc8de570
tanqiul <liutanqiu@gmail.com>
2020-04-05 20:34:08 -0700

    commit by start.spring.io

---
+++ src/main/java/com/deepwaterl/springreacttypescriptboilerplate/SpringReactTypescriptBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.deepwaterl.springreacttypescriptboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTypescriptBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactTypescriptBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepwaterl/springreacttypescriptboilerplate/SpringReactTypescriptBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepwaterl.springreacttypescriptboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactTypescriptBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
