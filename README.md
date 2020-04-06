2ffe30ca9096d2a0afe146b1fbe71d861bcd46cd
tanqiul <liutanqiu@gmail.com>
2020-04-05 22:54:09 -0700

    coordinate frontend and backend in production build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-react-typescript-boilerplate
 A boilerplate for spring boot web application with a frontend developed in react &amp; typescript
+
+Combining [kantega's approach](https://github.com/kantega/react-and-spring) with this [react-typescript0-starter](https://github.com/deepwaterL/react-typescript-boilerplate). 
--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -4,13 +4,30 @@ import '../styles/index.css';
 
 export interface IAppProps {}
 
-export interface IAppState {}
+export interface IAppState {
+	response: string
+}
 
 class App extends React.PureComponent<IAppProps, IAppState> {
+
+	constructor(props: IAppProps) {
+		super(props);
+		this.state = {response: 'Loading'};
+	}
+
+	componentDidMount(): void {
+		fetch('api/hello')
+			.then(response => response.text())
+			.then(text => {
+				this.setState({response: text});
+			});
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Hello World!</h1>
+				<p>{this.state.response}</p>
 			</div>
 		);
 	}
